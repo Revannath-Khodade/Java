469b633e1b9fd2822053630f20ad173dd62d8c60
Revannath Khodade <revannathkhodade01@gmail.com>
2025-09-26 15:30:54 +0530

    Add files via upload

---
+++ 5-swapping-numbers-using-anothr-variable.java
@@ -0,0 +1,15 @@
+class Main 
+{
+    public static void main(String args[])
+    {
+        int a=3 , b=5 , c;
+        
+        System.out.println("A= "+a+"B= "+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("A= "+a+"B= "+b);
+        
+        
+    }
+}
